d8257b881fb06486fc22ae740ce58059b1075103
SHLCW <dmwguy@gmail.com>
2016-04-03 15:29:57 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+con = sqlite3.connect('data.db')
+c = con.cursor()
+c.execute('''
+CREATE TABLE books (
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    pub_id INTEGER,
+    author TEXT,
+    mediatype_id INTEGER,
+    weight REAL
+)
+CREATE TABLE publishers (
+    pub_id INTEGER PRIMARY KEY,
+    pub_name TEXT,
+    tier INTEGER
+)
+CREATE TABLE mediatypes (
+    mediatype_id INTEGER PRIMARY KEY,
+    mediatype_name TEXT
+)
+CREATE TABLE listings (
+    listing_id INTEGER PRIMARY KEY,
+    vendor_id INTEGER,
+    book_id INTEGER,
+    price REAL
+)
+CREATE TABLE vendors (
+    vendor_id INTEGER PRIMARY KEY,
+    vendor_name TEXT
+INSERT
+''')
