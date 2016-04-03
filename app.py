572b3409917994eca89074b275b0f58115aa158f
chuckwolf <wolf.chuck55@gmail.com>
2016-04-03 17:03:39 -0400

    Fixed SQL Syntax and corrected execute statement

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect('data.db')
 c = con.cursor()
-c.execute('''
+c.executescript('''
 CREATE TABLE books (
     book_id INTEGER PRIMARY KEY,
     title TEXT,
@@ -10,24 +10,24 @@ CREATE TABLE books (
     author TEXT,
     mediatype_id INTEGER,
     weight REAL
-)
+);
 CREATE TABLE publishers (
     pub_id INTEGER PRIMARY KEY,
     pub_name TEXT,
     tier INTEGER
-)
+);
 CREATE TABLE mediatypes (
     mediatype_id INTEGER PRIMARY KEY,
     mediatype_name TEXT
-)
+);
 CREATE TABLE listings (
     listing_id INTEGER PRIMARY KEY,
     vendor_id INTEGER,
     book_id INTEGER,
     price REAL
-)
+);
 CREATE TABLE vendors (
     vendor_id INTEGER PRIMARY KEY,
     vendor_name TEXT
-INSERT
+);
 ''')
